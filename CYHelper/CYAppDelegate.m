8f3bef790b76ece1cc18c5811ce57aa01e658033
lancy <lancy1014@gmail.com>
2012-11-26 21:56:12 +0800

    remove useless code

--- CYHelper/CYAppDelegate.m
+++ CYHelper/CYAppDelegate.m
@@ -13,11 +13,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-        UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-        splitViewController.delegate = (id)navigationController.topViewController;
-    }
 
     return YES;
 }
