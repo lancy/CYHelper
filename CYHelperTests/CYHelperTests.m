709ec0ab1ca6cc9c5d684f2bc6ffb17107f3e6f4
lancy <lancy1014@gmail.com>
2012-11-26 12:38:26 +0800

    add: NSDate helper, md5 support

---
+++ CYHelperTests/CYHelperTests.m
@@ -0,0 +1,32 @@
+//
+//  CYHelperTests.m
+//  CYHelperTests
+//
+//  Created by Lancy on 11/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CYHelperTests.h"
+#import "NSData+CYHelper.h"
+
+@implementation CYHelperTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CYHelperTests");
+}
+
+@end
